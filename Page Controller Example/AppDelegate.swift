75833e7ff8c1bd87b840cfed18ce789b942de1a4
Wallace Wang <wallacewang@wallaces-imac.t-mobile.com>
2017-01-22 19:56:28 -0800

    Initial Commit

---
+++ Page Controller Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Page Controller Example
+//
+//  Created by Wallace Wang on 1/22/17.
+//  Copyright © 2017 Wallace Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
